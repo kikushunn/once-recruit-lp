46570ce2e8e06542ba3c6dfc852a64496da82256
菊池周 <kikuchiamane@kikuchiamanenoMacBook-Air.local>
2026-05-21 11:53:04 +0900

    force dynamic notion data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getRecruitStores, getInterviews } from "@/lib/notion";
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
   const stores = await getRecruitStores();
   const interviews = await getInterviews();
