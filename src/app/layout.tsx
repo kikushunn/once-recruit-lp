c9eb0451ed9093811f0008e8bc84aec83cdcc873
菊池周 <kikuchiamane@Mac.lan>
2026-05-22 20:31:49 +0900

    add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +28,28 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+  <Script
+    id="gtm"
+    strategy="afterInteractive"
+  >
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];
+      w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5GVR32C2');
+    `}
+  </Script>
+
+  {children}
+
+</body>
     </html>
   );
 }
